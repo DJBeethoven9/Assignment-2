3f30e8917c5177f6fa0543c18720688ad924453d
DJBeethoven <87830367+EatSumMeatBro@users.noreply.github.com>
2024-11-30 19:03:36 +0300

    final edit

--- UOB-API.php
+++ UOB-API.php
@@ -1,3 +1,7 @@
+<!--- 
+Sayed Jaafar Sadeq      20184311
+Maitham Ali Isa         20186039
+--->
 <?php
 $URL = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
 $response = file_get_contents($URL);
